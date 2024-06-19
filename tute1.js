3ad803cdd6df2dd7126b7b586a48126853bcf834
itsniharsharma <itsniharsharmas@gmail.com>
2024-06-19 13:40:44 +0530

    updated

---
+++ tute1.js
@@ -0,0 +1,9 @@
+// AJAX Request- Asynchronous JavaScript and XML helps to do changes dynamically without reloading the page
+// say codewhary send AJAX Xml req to me , i have fcbok lognd then it can get my all data
+// to prevent that brousers have same origin policy, like you cant have a handshake like that.
+// but it could maybe possible by including certain hardders.
+//JavaScript is maintained by ECMA, It push updates for javaScript
+
+var name="Jake";  // var make it a global variable
+let name="Nihar"; //let will make it a local variable
+
